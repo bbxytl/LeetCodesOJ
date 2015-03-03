fdc84e34b39e5c5f5bed76e551d304f80e5afae3
bbxytl <bbxytl@gmail.com>
2015-03-03 15:18:07 +0800

    Median of Two Sorted Arrays

--- Algorithms/Median of Two Sorted Arrays/Median of Two Sorted Arrays.cpp
+++ Algorithms/Median of Two Sorted Arrays/Median of Two Sorted Arrays.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    double findMedianSortedArrays(int A[], int m, int B[], int n) {
+    double findMedianSortedArrays_1(int A[], int m, int B[], int n) {
         if(m==0&&n==0)return 0;
         if(m+n==1) return m==1?A[0]:B[0];
         int mid=0,next=0,cont=0;
@@ -32,5 +32,28 @@ public:
         }
         if((m+n)%2!=0)return mid;
         return (mid+next)/2.0;
+    }
+
+    int getkth(int s[], int m, int l[], int n, int k){
+        // let m <= n
+        if (m > n)
+        return getkth(l, n, s, m, k);
+        if (m == 0)
+            return l[k - 1];
+        if (k == 1)
+            return min(s[0], l[0]);
+
+        int i = min(m, k / 2), j = min(n, k / 2);
+        if (s[i - 1] > l[j - 1])
+            return getkth(s, m, l + j, n - j, k - j);
+        else
+            return getkth(s + i, m - i, l, n, k - i);
+        return 0;
+    }
+
+    double findMedianSortedArrays_2(int A[], int m, int B[], int n) {
+        int l = (m + n + 1) >> 1;
+        int r = (m + n + 2) >> 1;
+        return (getkth(A, m ,B, n, l) + getkth(A, m, B, n, r)) / 2.0;
     }
 };
--- Algorithms/Median of Two Sorted Arrays/README.md
+++ Algorithms/Median of Two Sorted Arrays/README.md
@@ -1,5 +1,7 @@
 #[Median of Two Sorted Arrays](https://oj.leetcode.com/problems/median-of-two-sorted-arrays/)
 
 There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be **`O(log (m+n))`**.
-- **Median -- 49ms(cpp)**
+- **Median_1 -- 49ms(cpp)**
+- [**Median_2 -- 53ms(cpp)**](https://oj.leetcode.com/discuss/9265/share-my-simple-o-log-m-n-solution-for-your-reference)
+    - 使用算法导论中的**Kth算法**。
 
